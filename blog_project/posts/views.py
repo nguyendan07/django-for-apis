5ac8b2e06b35924f4beb2640200fa2b22fcfb7c1
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-06-25 18:23:43 +0700

    Create two views and routes url. PostListView and PostDetailView

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostListView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
