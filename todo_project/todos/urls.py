5167a57cf0f0ecfc7365b8ab560a90be8173574b
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-06-25 11:09:24 +0700

    Todo API

---
+++ todo_project/todos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import TodoList, TodoDetail
+
+
+urlpatterns = [
+    path("", TodoList.as_view(), name="todo-list"),
+    path("<int:pk>/", TodoDetail.as_view(), name="todo-detail"),
+]
