bc8b63b293c58bdbecc0726ac33d54e50a013a1f
unknown <dannv@DESKTOP-6GMM97I.officehn.local>
2020-09-01 17:59:59 +0700

    Custom permission

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
