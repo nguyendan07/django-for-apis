9c00b5de07fc0f79cc4212fa93913b669924601b
Nguyen Van Dan <nguyenvdan07@gmail.com>
2020-09-23 18:56:22 +0700

    Implementing token authentication

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd-party apps
     'rest_framework',
+    'rest_framework.authtoken',
+
+    # local
     'posts.apps.PostsConfig',
 ]
 
@@ -130,6 +135,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ]
 }
 
