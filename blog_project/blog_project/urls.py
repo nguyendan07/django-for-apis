9cd0756e44c410a07e99655be50f83da74909e30
Nguyen Van Dan <nguyendan@hotmail.com.vn>
2020-09-25 18:14:42 +0700

    Generate real Swagger/OpenAPI 2.0

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -17,11 +17,26 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view as get_openapi_schema_view
+from drf_yasg import openapi
 
 API_TITLE = 'Blog API'
 API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
 
 schema_view = get_schema_view(title=API_TITLE)
+openapi_schema_view = get_openapi_schema_view(
+    openapi.Info(
+        title=API_TITLE,
+        default_version='v1',
+        description=API_DESCRIPTION,
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +45,11 @@ urlpatterns = [
     path('api/v1/rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
-    path('schema/', schema_view)
+    path('schema/', schema_view),
+    path('swagger/',
+         openapi_schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/',
+         openapi_schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
